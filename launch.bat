768e4d2c4490017cc30ae7ed4cf1e700e88730ad
Taldes <98669248+Taldes@users.noreply.github.com>
2022-01-30 05:24:13 +0300

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -2,7 +2,7 @@
 cls
 start ChromeDriver.exe
 cls
-title Alucard Launcher
+title Taldes Launcher
 py -3.8 Main.py
 echo RIP I don't want to make a loop for a user who doesn't want to understand :)
 pause
